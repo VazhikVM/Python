a69870ace968f65e13181477b39a1ac4fb8c03f0
Виталий <v.vazhinskiy@yandex.ru>
2020-12-14 13:24:57 +0300

    Задание 1

---
+++ 1.py
@@ -0,0 +1,9 @@
+onevar = 1
+twovar = 2
+print(onevar, twovar)
+name = None
+onevar = input("Введите число: ")
+twovar = input("введите еще одно число: ")
+name = input("Введите Ваше имя: ")
+print("Привет,", name)
+
